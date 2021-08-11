a6bcecdc51fbb40298fc45ff32362602e34d1006
NicolasCoelho <nicolas_mansan@hotmail.com>
2021-08-11 01:39:48 -0300

    Teste de passwor do cokkie

--- util/session.js
+++ util/session.js
@@ -2,10 +2,10 @@ import { withIronSession } from "next-iron-session";
 
 export default function session(handler) {
     return withIronSession(handler, {
-        password: process.env.SECRET_COOKIE_PASSWORD,
+        password: "Teste123",
         cookieName: 'sessionID',
         cookieOptions: {
             secure: process.env.NODE_ENV === 'production' ? true : false,
-        },
+        }
     })
 }
