5888aae3fd3c1828e5333b87cf2380848df473b2
NicolasCoelho <nicolas_mansan@hotmail.com>
2021-08-12 18:17:45 -0300

    Robots.txt e sitemap

---
+++ pages/api/sitemap.js
@@ -0,0 +1,42 @@
+import { connectToDatabase } from '../../util/mongodb'
+ 
+import { SitemapStream, streamToPromise } from 'sitemap';
+
+export default async (req, res) => {
+    try {
+        const smStream = new SitemapStream({
+            hostname: `https://${req.headers.host}`,
+            cacheTime: 600000,
+        });
+
+        smStream.write( {url: `/servicos`, changefreq: 'daily', priority: 0.9} );
+        smStream.write( {url: `/posts`, changefreq: 'daily', priority: 0.9} );
+        smStream.write( {url: `/produtos`, changefreq: 'daily', priority: 0.9} );
+        smStream.write( {url: `/contato`, changefreq: 'daily', priority: 0.9} );
+
+        const { db } = await connectToDatabase()
+
+        const posts = await db.collection('posts').aggregate({$project: { _id:0, url: 1 }}).toArray();
+
+        posts.forEach(post => {
+            smStream.write({
+                url: `/post/${post.url}`,
+                changefreq: 'daily',
+                priority: 0.9
+            });
+        });
+
+        smStream.end();
+
+        const sitemapOutput = (await streamToPromise(smStream)).toString();
+
+        res.writeHead(200, {
+            'Content-Type': 'application/xml'
+        });
+
+        res.end(sitemapOutput);
+    } catch(e) {
+        console.log(e)
+        res.send(JSON.stringify(e))
+    }
+}
