629421d5036097462debe755b0d96250051e0443
NicolasCoelho <nicolas_mansan@hotmail.com>
2021-08-16 21:31:46 -0300

    Página de produtos e componente de produto

--- components/product/index.js
+++ components/product/index.js
@@ -1,18 +1,19 @@
-import Link from "next/link";
 
 export default function Product({...props}) { 
     
     return (
         <li className="product-inline">
-            <Link href={`/produto/${props.url}`}>
-                <a>
-                    <img src={props.thumbnail} alt={`${props.title}-imagem`}></img>
-                    <div className="product-title">{props.name}</div>
-                    <div>{props.resume}</div>
-                    <div>{props.price}</div>
-                    <div>Mais detalhes</div>
-                </a>
-            </Link>
+            <a href={`${props.link}`} target="_BLANK" rel="noreferrer noopener">
+                <div className="product-image">
+                    <div className="product-image-wrapper">
+                        <img src={props.thumbnail} alt={`${props.title}-imagem`}></img>
+                    </div>
+                </div>
+                <div className="product-title">{props.name}</div>
+                <div className="product-resume">{props.resume}</div>
+                <div className="product-price">{props.price}</div>
+                <button className="px-5 py-3 font-bold text-white bg-primary">Comprar</button>
+            </a>
         </li>
     )
 }
--- pages/produtos.js
+++ pages/produtos.js
@@ -19,7 +19,7 @@ export default function Produtos({produtos}) {
                 <h1 className="my-8 font-bold">Produtos Recomendados</h1>
                 <p className="text-lg mb-8">Os melhores produtos para manter a sua vida organizada!</p>
                 
-                <ul className="flex flex-wrapper justify-between">
+                <ul className="flex flex-wrap justify-between">
                     {produtos.map( (p,i) => <Produto {...p} key={`produto-${i}`} /> )}
                 </ul>
             </main>
@@ -34,7 +34,7 @@ export async function getStaticProps() {
     let produtos = await db
     .collection("produtos")
     .find({})
-    .project({url: 1, name: 1, price: 1, createdAt: 1, thumbnail: 1, url: 1, resume: 1})
+    .project({url: 1, name: 1, price: 1, createdAt: 1, thumbnail: 1, link: 1, resume: 1})
     .project({_id: 0, description: 0, metaDescription: 0, metaKeywords: 0, image: 0, pageTitle: 0})
     .sort({ metacritic: -1 })
     .toArray()
