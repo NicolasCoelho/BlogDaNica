a643ad82448364e04f86e17906850fa9d126e2a5
NicolasCoelho <nicolas_mansan@hotmail.com>
2021-08-18 20:09:01 -0300

    Ajuste para clique no card de post

--- components/card/index.js
+++ components/card/index.js
@@ -1,18 +1,23 @@
-import Link from "next/link"
+import { useRouter } from 'next/router'
 import formatDate from "../../util/formatDate"
 
 export default function Card(props) {
+    const router = useRouter()
+
+    function goTo (){
+        router.push(`/post/${props.url}`)
+    }
 
     function getThumb() {
         return `url("${props.thumbnail}")`
     }
     return (
-        <article className="flex post-card w-auto flex-col sm:flex-row">
+        <article className="flex post-card w-auto flex-col sm:flex-row" onClick={goTo}>
             <figure className="w-full h-72 sm:h-auto sm:w-5/12 bg-cover bg-no-repeat bg-center" style={{ backgroundImage: getThumb() }}></figure>
             <div className="w-full sm:w-7/12 p-8 h-auto border border-gray-300 text-center sm:text-left">
                 <header>
                     <time className="text-sm capitalize">{ formatDate(props.createdAt, 2) }</time>
-                    <h3 className="text-2xl font-bold my-4 sm:my-6 "> <Link href={`/post/${props.url}`}><a>{props.title}</a></Link></h3>
+                    <h3 className="text-2xl font-bold my-4 sm:my-6 ">{props.title}</h3>
                 </header>
                 <p> 
                     {props.resume}
